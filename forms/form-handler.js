74e5cffd64fbd2a4f304b7dd1b02af9199bca4bc
eEfia <efianyamekye46@gmail.com>
2026-08-19 09:43:47 +0100

    Add files via upload

--- forms/form-handler.js
+++ forms/form-handler.js
@@ -4,7 +4,7 @@ APRILS SIGNATURE
 FORMS CONTROLLER
 =========================================================
 
-This file prepares the website forms forsub submission.
+This file prepares the website forms for submission.
 
 Forms covered:
 1. Order / Quote Requests
