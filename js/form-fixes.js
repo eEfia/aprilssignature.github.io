c296c4d7011ef64188b6bedc74238e206562c364
eEfia <efianyamekye46@gmail.com>
2026-08-30 04:09:52 +0100

    Add files via upload

--- js/form-fixes.js
+++ js/form-fixes.js
@@ -10,7 +10,7 @@ PUBLIC FORM FIXES
 (function () {
 
     function getSupabase() {
-        return window.aprilsSupabase || window.AprilsSupase || null;
+        return window.aprilsSupabase || window.AprilsSupabase || null;
     }
 
     function waitForSupabase(timeout = 15000) {
