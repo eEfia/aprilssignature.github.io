da96cf8ab784b5c5f3ccc89eb4e075ea68a0c151
eEfia <efianyamekye46@gmail.com>
2026-08-31 13:36:29 +0100

    Update form-fixes.js

--- js/form-fixes.js
+++ js/form-fixes.js
@@ -10,7 +10,7 @@ PUBLIC FORM FIXES
 (function () {
 
     function getSupabase() {
-        return window.aprilsSupabase || window.AprilsSupabase || null;
+        return window.aprilsSupase || window.AprilsSupabase || null;
     }
 
     function waitForSupabase(timeout = 15000) {
