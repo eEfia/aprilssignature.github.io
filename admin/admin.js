5de50613a418caf7252febdc460b1ed9cb425b5d
eEfia <efianyamekye46@gmail.com>
2026-09-01 00:00:59 +0100

    Update admin.js

--- admin/admin.js
+++ admin/admin.js
@@ -7,7 +7,7 @@
 
 let db = null;
 
-const DEFAULT_GALLERY_COLLECTIONS = [
+const DEFAULT_GALLERY_COLLECONS = [
     "Streetwear Collection",
     "Rhinestone Embellishment",
     "Fashion Creations",
