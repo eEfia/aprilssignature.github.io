3c8f48bc7783ae183dc74944e81cf3814593f67b
eEfia <efianyamekye46@gmail.com>
2026-09-03 10:02:45 +0100

    Update script.js

--- js/script.js
+++ js/script.js
@@ -15,7 +15,7 @@
 
 function escapeHTML(value) {
     return String(value ?? "")
-        .replace(/&/g, "&amp;")
+        .replace(/&/g, p;")
         .replace(/</g, "&lt;")
         .replace(/>/g, "&gt;")
         .replace(/"/g, "&quot;")
