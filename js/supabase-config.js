479c367f8424c5459668f3f4c3fefabff41afef7
eEfia <efianyamekye46@gmail.com>
2026-09-04 10:22:34 +0100

    Update supabase-config.js

--- js/supabase-config.js
+++ js/supabase-config.js
@@ -2,7 +2,6 @@ window.APRILS_SUPABASE_CONFIG = {
 
     url: "https://dftljtpebiozagvclfwv.supabase.co",
 
-    publishableKey:
-        "sb_publishable_SMSVcooUHVx6Vi2FzFNP9A_VrFcTaZm"
+    publim"
 
 };
